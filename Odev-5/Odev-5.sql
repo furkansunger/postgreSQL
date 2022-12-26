8acf9dc1de97ea4b18280ec95a7634ecee5ac5b5
Furkan <furkan.sunger@outlook.com>
2022-12-26 15:32:20 +0300

    Odev-5 done

---
+++ Odev-5/Odev-5.sql
@@ -0,0 +1,5 @@
+select title from film where title like '%n' order by length limit 5
+
+select title from film where title like '%n' order by length desc limit 5
+
+select store_id, last_name from customer where store_id = '1' order by last_name desc limit 4
